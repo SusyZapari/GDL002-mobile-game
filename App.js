b3c66d2629f08ffc2e95bac3e15c6addb2c54ee7
Susana Zapari <susy.zapari@gmail.com>
2019-06-20 16:20:25 -0500

    Adding images to card

--- App.js
+++ App.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Card from './components/Card';
+import Header from './components/Header';
+//import './App.css';
+//import Board from './components/Board';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Card type='Manzana'></Card> 
-      <Card type='Piña'></Card>
-      <Card type='Pera'></Card>
-      <Card type='Nanchis'></Card>
-      <Card type='Plátano'></Card>
-      <Card type='Pera'></Card>
-      <Card type='Durazno'></Card>
-      <Card type='Uvas'></Card>
-      <Card type='Durazno'></Card>
-      <Card type='Manzana'></Card>
-      <Card type='Uvas'></Card>
-      <Card type='Nanchis'></Card>
-      <Card type='Piña'></Card>
+    <View>
+      <Card type='manzana'></Card> 
+      <Card type='pina'></Card>
+      <Card type='pera'></Card>
+      <Card type='banana'></Card>
+      <Card type='cerezas'></Card>
+      <Card type='fresa'></Card>
+      <Card type='kiwi'></Card>
+      <Card type='mango'></Card>
+      <Card type='naranja'></Card>
+      <Card type='sandia'></Card>
+      <Card type='uvas'></Card>
+      <Card type='blueberry'></Card>
 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/*class App extends Component {
+  render () {
+    return (
+      <View className="App">
+      <Header/>
+      <Board/>
+      </View>
+    )
+  }
+}*/
+//<TouchableOpacity style={styles.CardStyle} activeOpacity={0.5}>
+  //  <Image
+    // source={require('./Images/banana.png')}
+     //style={styles.ImageIconStyle}
+    ///>
+    //<View style={styles.SeparatorLine} />
+    //<Text style={styles.TextStyle}> Iniciar </Text>
+//</TouchableOpacity>
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
--- components/Card.js
+++ components/Card.js
@@ -1,36 +1,75 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+//import './Card.css';
 
 class Card extends Component {
     constructor() {
         super();
         this.state = {
-            cardText: '?'
+            cardImage: 'marcapersonal'
         }
     }
     flipCard= ()=> {
-        let nextCardText = '';
-        if (this.state.cardText == '?'){
-            nextCardText = this.props.type;
+        let nextCardImage = '';
+        if (this.state.cardImage == 'marcapersonal'){
+            nextCardImage = this.props.type;
         }
         else {
-            nextCardText = '?';
+            nextCardImage = 'marcapersonal';
         }
         this.setState({
-            cardText: nextCardText
+            cardImage: nextCardImage
           });
     }
     render (){
         return (
-            <Button 
-            onPress={this.flipCard}
-            title={this.state.cardText}
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-          />
+        <TouchableOpacity onPress={this.flipCard}>
+            <Image
+                style={styles.button}
+                source={images[this.state.cardImage]}/>
+        </TouchableOpacity>
+       
         );
     }
 }
+const images = {
+    'manzana': require('../Images/manzana.png'),
+    'pera': require('../Images/pera.png'),
+    'blueberry': require('../Images/blueberry.png'),
+    'cerezas': require('../Images/cerezas.png'),
+    'fresa': require('../Images/fresa.png'),
+    'kiwi': require('../Images/kiwi.png'),
+    'mango': require('../Images/mango.png'),
+    'naranja': require('../Images/naranja.png'),
+    'pina': require('../Images/pina.png'),
+    'sandia': require('../Images/sandia.png'),
+    'uvas': require('../Images/uvas.png'),
+    'banana': require('../Images/banana.png'),
+    'marcapersonal': require('../Images/marcapersonal.png'),
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 100,
+    height: 100,
+
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10
+  },
+  countText: {
+    color: '#FF00FF'
+  }
+})
 
 export default Card;
 
